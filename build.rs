bcd0c818a4d24a48eddb2a18ea2e6ada78c54df7
Vsevolod Velichko <torkve@yandex-team.ru>
2014-11-07 00:16:18 +0300

    build fix

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 extern crate "pkg-config" as pkg_config;
 
 fn main() {
-    pkg_config::find_library_opts("uuid", &Options{statik: true, atleast_version: None}).unwrap()
+    pkg_config::find_library_opts("uuid", &pkg_config::Options{statik: true, atleast_version: None}).unwrap()
 }
